d59926e09e4c0dca7662d96b9c255f8dd63b3004
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2017-10-04 06:54:31 +0100

    Remove worker manager. Not needed

--- src/main/java/ai/grakn/redismock/commands/RO_brpoplpush.java
+++ src/main/java/ai/grakn/redismock/commands/RO_brpoplpush.java
@@ -1,27 +1,19 @@
 package ai.grakn.redismock.commands;
 
 import ai.grakn.redismock.RedisBase;
-import ai.grakn.redismock.RedisClient;
 import ai.grakn.redismock.Response;
 import ai.grakn.redismock.Slice;
 import ai.grakn.redismock.SliceParser;
-import ai.grakn.redismock.util.WorkerManager;
-import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 import static ai.grakn.redismock.Utils.convertToLong;
 
 class RO_brpoplpush extends RO_rpoplpush {
-    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(RO_unsubscribe.class);
-    private final RedisClient client;
-
-    RO_brpoplpush(RedisBase base, RedisClient client, List<Slice> params) {
+    RO_brpoplpush(RedisBase base, List<Slice> params) {
         //NOTE: The minimum number of arguments is 1 because this mock is used for brpoplpush as well which takes in 3 arguments
         super(base, params, 3);
-        this.client = client;
     }
 
     @Override
@@ -29,35 +21,24 @@ class RO_brpoplpush extends RO_rpoplpush {
         Slice source = params().get(0);
         long timeout = convertToLong(params().get(2).toString());
 
-        WorkerManager.runJob(() -> {
-            //TODO: Active block dumb.
-            long currentSleep = 0L;
-            long count = 0L;
-            while(count == 0L && currentSleep < timeout * 1000){
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                currentSleep = currentSleep + 10;
-                count = getCount(source);
-            }
-
-            Slice response;
-            if(count != 0){
-                response = super.execute();
-            } else {
-                response = Response.NULL;
-            }
-
+        //TODO: Remove active block dumb.
+        long currentSleep = 0L;
+        long count = 0L;
+        while(count == 0L && currentSleep < timeout * 1000){
             try {
-                client.sendResponse(Response.clientResponse("brpoplpush", response));
-            } catch (IOException e) {
-                LOG.error("Unable to respond to brpoplpush", e);
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-        });
+            currentSleep = currentSleep + 100;
+            count = getCount(source);
+        }
 
-        return Response.SKIP;
+        if(count != 0){
+            return super.execute();
+        } else {
+            return Response.NULL;
+        }
     }
 
     private long getCount(Slice source){
