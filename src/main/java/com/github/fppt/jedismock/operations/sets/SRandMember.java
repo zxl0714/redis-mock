938f7f2e660c29f72d8820edc73b953b0a8177ca
Ivan Ponomarev <iponomarev@mail.ru>
2023-05-10 21:14:46 +0300

    implement srandmember (#167)
    * implement srandmember

---
+++ src/main/java/com/github/fppt/jedismock/operations/sets/SRandMember.java
@@ -0,0 +1,59 @@
+package com.github.fppt.jedismock.operations.sets;
+
+import com.github.fppt.jedismock.datastructures.RMSet;
+import com.github.fppt.jedismock.datastructures.Slice;
+import com.github.fppt.jedismock.operations.AbstractRedisOperation;
+import com.github.fppt.jedismock.operations.RedisCommand;
+import com.github.fppt.jedismock.server.Response;
+import com.github.fppt.jedismock.storage.RedisBase;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+
+import static com.github.fppt.jedismock.Utils.convertToInteger;
+
+@RedisCommand("srandmember")
+public class SRandMember extends AbstractRedisOperation {
+    public SRandMember(RedisBase base, List<Slice> params) {
+        super(base, params);
+    }
+
+    @Override
+    protected Slice response() {
+        Slice key = params().get(0);
+        RMSet set = base().getSet(key);
+        if (set == null) {
+            return Response.NULL;
+        }
+        int number;
+        if (params().size() > 1) {
+            number = convertToInteger(params().get(1).toString());
+        } else {
+            number = 1;
+        }
+        //TODO: more effective algorithms should be used here,
+        //avoiding conversion of set to list, shuffling all the elements etc.
+        List<Slice> list = new ArrayList<>(set.getStoredData());
+        if (number == 1) {
+            int index = ThreadLocalRandom.current().nextInt(list.size());
+            return Response.bulkString(list.get(index));
+        } else if (number > 1) {
+            Collections.shuffle(list);
+            return Response.array(
+                    list.stream()
+                            .map(Response::bulkString)
+                            .limit(number)
+                            .collect(Collectors.toList()));
+        } else {
+            List<Slice> result =
+                    ThreadLocalRandom.current().ints(-number, 0, list.size())
+                    .mapToObj(list::get)
+                    .map(Response::bulkString)
+                    .collect(Collectors.toList());
+            return Response.array(result);
+        }
+    }
+}
---
+++ src/test/java/com/github/fppt/jedismock/comparisontests/sets/SRandMemberTest.java
@@ -0,0 +1,88 @@
+package com.github.fppt.jedismock.comparisontests.sets;
+
+import com.github.fppt.jedismock.comparisontests.ComparisonBase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import redis.clients.jedis.Jedis;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@ExtendWith(ComparisonBase.class)
+public class SRandMemberTest {
+    @BeforeEach
+    public void setUp(Jedis jedis) {
+        jedis.flushAll();
+    }
+
+    @TestTemplate
+    void randMemberReturnsARandomElementOfTheSet(Jedis jedis) {
+        Collection<String> set = Arrays.asList("a", "b", "c");
+        jedis.sadd("foo", set.toArray(new String[0]));
+        Set<String> usedElements = new HashSet<>();
+        for (int i = 0; i < 5000; i++) {
+            String member = jedis.srandmember("foo");
+            Assertions.assertTrue(set.contains(member));
+            usedElements.add(member);
+        }
+        Assertions.assertEquals(new HashSet<>(set), usedElements);
+    }
+
+    @TestTemplate
+    void randMemberReturnsOnlyElementOfTheSet(Jedis jedis) {
+        Set<String> set = Collections.singleton("d");
+        jedis.sadd("foo", set.toArray(new String[0]));
+        for (int i = 0; i < 100; i++) {
+            Assertions.assertEquals("d", jedis.srandmember("foo"));
+        }
+    }
+
+    @TestTemplate
+    void randMemberReturnsNullForAnEmptySet(Jedis jedis) {
+        for (int i = 0; i < 100; i++) {
+            Assertions.assertNull(jedis.srandmember("foo"));
+        }
+    }
+
+    @TestTemplate
+    void randMemberReturnsDistinctElements(Jedis jedis) {
+        Collection<String> set = Arrays.asList("a", "b", "c", "d", "e");
+        jedis.sadd("foo", set.toArray(new String[0]));
+        for (int i = 0; i < 1000; i++) {
+            List<String> members = jedis.srandmember("foo", 3);
+            Assertions.assertTrue(set.containsAll(members));
+            Assertions.assertEquals(3, new HashSet<>(members).size());
+        }
+    }
+
+    @TestTemplate
+    void randMemberReturnsAllElements(Jedis jedis) {
+        Collection<String> set = Arrays.asList("a", "b", "c", "d", "e");
+        jedis.sadd("foo", set.toArray(new String[0]));
+        List<String> members = jedis.srandmember("foo", 10);
+        Assertions.assertEquals(new HashSet<>(set), new HashSet<>(members));
+    }
+
+    @TestTemplate
+    void randMemberReturnsNoElements(Jedis jedis) {
+        Collection<String> set = Arrays.asList("a", "b", "c", "d", "e");
+        jedis.sadd("foo", set.toArray(new String[0]));
+        List<String> members = jedis.srandmember("foo", 0);
+        Assertions.assertEquals(0, members.size());
+    }
+
+    @TestTemplate
+    void randMemberReturnsRepeatedElements(Jedis jedis) {
+        Collection<String> set = Arrays.asList("a", "b", "c");
+        jedis.sadd("foo", set.toArray(new String[0]));
+        List<String> members = jedis.srandmember("foo", -100);
+        Assertions.assertEquals(100, members.size());
+        Assertions.assertTrue(set.containsAll(members));
+    }
+}
